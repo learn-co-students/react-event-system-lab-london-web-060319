df19901febec51df1e9aac7b7ba341a61a00452d
Amy Quinn <amyequinn@hotmail.co.uk>
2019-08-06 14:21:06 +0100

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+export default class EyesOnMe extends React.Component{
+
+  eyesonme = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  blurry = () => {
+    console.log('Good!')
+  }
+
+  render() {
+    return(
+      <button onFocus={this.blurry} onBlur={this.eyesonme}> Click</button>
+    )
+  }
+
+}
